0dbc44de6ba9655a6642573ea61131bb1a421640
sbx320 <sbx320@sbx320.net>
2017-06-22 02:37:25 +0200

    Initial state from SteamRE/SteamKit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProtobufDumper
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Environment.ExitCode = 0;
+
+            if ( args.Length == 0 )
+            {
+                Console.WriteLine( "No target specified." );
+
+                Environment.ExitCode = -1;
+                return;
+            }
+
+            string target = args[ 0 ];
+            string output = ( ( args.Length > 1 ) ? args[ 1 ] : null );
+
+            ImageFile imgFile = new ImageFile( target, output );
+
+            try
+            {
+                imgFile.Process();
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine( "Unable to process file: {0}", ex.Message );
+                Console.WriteLine( ex.StackTrace );
+                Environment.ExitCode = -1;
+            }
+        }
+    }
+}
